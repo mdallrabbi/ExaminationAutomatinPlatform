4258d623ab0ee8074fd73353f8cad9316d965680
mdallrabbi <dreamer.rabbi@gmail.com>
2019-01-27 01:51:06 +0600

    Template Intigration & Model to HTML connection

--- ExamAutomationPlatform/settings.py
+++ ExamAutomationPlatform/settings.py
@@ -126,3 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# STATICFILES_DIRS=(
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
+# #PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
